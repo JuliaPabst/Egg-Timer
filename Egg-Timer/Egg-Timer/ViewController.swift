d9302a3c711539c80f97f18b25865d3c4ca40d2f
Jules <ju.pabst@gmail.com>
2023-12-29 18:39:58 +0100

    added sound

--- Egg-Timer/Egg-Timer/ViewController.swift
+++ Egg-Timer/Egg-Timer/ViewController.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 import Foundation
+import AVFoundation
+
 
 class ViewController: UIViewController {
     
+    var player: AVAudioPlayer!
    
     let eggTimes: [String: Float] = ["Soft": 200, "Medium": 420, "Hard": 720]
-    
     
     @IBOutlet var topLabel: UILabel!
     var timer: Timer?
@@ -22,6 +24,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
        super.viewDidLoad()
     }
+
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        progressBar.progress = 0.0
+        
+        if(sender.tag == 0){
+            stopTimer()
+            startTimer(totalTime: eggTimes["Soft"]!)
+            topLabel.text = "You chose: Soft"
+            print(eggTimes["Soft"]!)
+        } else if(sender.tag == 1){
+            stopTimer()
+            startTimer(totalTime: eggTimes["Medium"]!)
+            topLabel.text = "You chose: Medium"
+            print(eggTimes["Medium"]!)
+        } else {
+            stopTimer()
+            startTimer(totalTime: eggTimes["Hard"]!)
+            topLabel.text = "You chose: Hard"
+            print(eggTimes["Hard"]!)
+        }
+    }
     
     func stopTimer() {
         timer?.invalidate()
@@ -29,44 +52,27 @@ class ViewController: UIViewController {
     }
 
     func startTimer(totalTime: Float) {
-        var secondsRemaining = totalTime
+        var secondsRemaining: Float = 0
        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
            guard let self = self else { return }
-           if secondsRemaining > 0 {
+           if secondsRemaining < totalTime {
+               secondsRemaining += 1
                print("\(secondsRemaining) seconds remaining")
-               progressBar.progress = 1 - secondsRemaining / totalTime
-               secondsRemaining -= 1
+               progressBar.progress = secondsRemaining / totalTime
            } else {
                self.timer?.invalidate()
                print("Timer completed")
                topLabel.text = "Done!"
+               playSound(soundName: "alarm_sound")
                stopTimer()
            }
            }
        }
-
-      
-
-    @IBAction func hardnessSelected(_ sender: UIButton) {
-        
-   
-        
-        if(sender.tag == 0){
-            stopTimer()
-            startTimer(totalTime: eggTimes["Soft"]!)
-            print(eggTimes["Soft"]!)
-        } else if(sender.tag == 1){
-            stopTimer()
-            startTimer(totalTime: eggTimes["Medium"]!)
-            print(eggTimes["Medium"]!)
-        } else {
-            stopTimer()
-            startTimer(totalTime: eggTimes["Hard"]!)
-            print(eggTimes["Hard"]!)
-        }
-        
-        
-      
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
     }
     
 }
